dbffdc1c9ba3df315898dc6ee260ff764ef7ccd7
Mirroy2003 <luis.eduardo.200325@gmail.com>
2025-11-28 03:20:05 -0500

    agregando: cors

---
+++ src/main/java/com/spotfinder/backend/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.spotfinder.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**")
+                        .allowedOrigins(
+                                "http://localhost:4200",                          // frontend en desarrollo
+                                "https://brave-mushroom-0031ada10.3.azurestaticapps.net" // frontend desplegado
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
